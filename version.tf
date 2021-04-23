2f1b842ade15d2300ae2603aa9db48fede04a5db
Mohamed Belgaied Hassine <60746935+belgaied2@users.noreply.github.com>
2021-04-23 12:04:49 +0200

    Update Kubernete provider version in version.tf

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "1.12.0"
+      version = "2.1.0"
     }
     helm = {
       source = "hashicorp/helm"
